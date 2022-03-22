31a16db9435b86a8a4e9aab9fc064cd1b157c2aa
liukanshan1024/liukanshan1024 <85719360+liukanshan1@users.noreply.github.com>
2022-03-22 11:12:01 +0800

    加入槽函数并连接信号

--- Windows/GUI/mainWindow.cpp
+++ Windows/GUI/mainWindow.cpp
@@ -1,30 +1,34 @@
-#include "classLib.h"
 #include "mainWindow.h"
-#include "mainLoop.h"
 mainWindow::mainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
 	ui.setupUi(this);
 	setWindowTitle(tr("监控面板"));
 
-
-
-	////添加菜单项，添加动作
-	//QAction* pNew = pFile->addAction("新建");
-	//connect(pNew, &QAction::triggered,
-	//	[=]()
-	//	{
-	//		emit stopLoop();
-	//	}
-	//);
-
+	connect(ui.log, SIGNAL(stateChanged(int)), this, SLOT(enableLogChanged()));
+	connect(ui.update, SIGNAL(stateChanged(int)), this, SLOT(enableUpdateChanged()));
+	connect(ui.dataLog, SIGNAL(stateChanged(int)), this, SLOT(dataLogChanged()));
+	connect(ui.cpu, SIGNAL(stateChanged(int)), this, SLOT(cpuUsageWarningChanged()));
+	connect(ui.memory, SIGNAL(stateChanged(int)), this, SLOT(memoryUsageWarningChanged()));
+	connect(ui.disk, SIGNAL(stateChanged(int)), this, SLOT(diskUsageWarningChanged()));
+	connect(ui.cpuLimit, SIGNAL(editingFinished()), this, SLOT(cpuUsageLimit()));
+	connect(ui.memoryLimit, SIGNAL(editingFinished()), this, SLOT(memoryUsageLimit()));
+	connect(ui.diskLimit, SIGNAL(editingFinished()), this, SLOT(diskUsageLimit()));
 	
-	loop=new mainLoop(nullptr, &myCPU, &myDisks, &myMemory, &myNetwork, &mySystem, &myUpdate);
+	loop = new mainLoop(nullptr, &myCPU, &myDisks, &myMemory, &myNetwork, &mySystem, &myUpdate);
 	//开始更新信息主循环
 	loop->start();
-	connect(this, &mainWindow::stopLoop, loop, &mainLoop::stopLoop);
-	
-	
+	connect(this, SIGNAL(stopLoop()), loop, SLOT(stopLoop()));
+	connect(loop, SIGNAL(sentCpuUsage(int)), this, SLOT(cpuUsageChanged(int)));
+	connect(loop, SIGNAL(sentDiskIO(io)), this, SLOT(diskIOChanged(io)));
+	connect(loop, SIGNAL(sentDiskRead(io)), this, SLOT(diskReadChanged(io)));
+	connect(loop, SIGNAL(sentDiskWrite(io)), this, SLOT(diskWriteChanged(io)));
+	connect(loop, SIGNAL(sentNetworkDownload(io)), this, SLOT(netDownloadChanged(io)));
+	connect(loop, SIGNAL(sentNetworkUpload(io)), this, SLOT(netUploadChanged(io)));
+	connect(loop, SIGNAL(sentRunningTime(runningTime)), this, SLOT(runningTimeChanged(runningTime)));
+	connect(loop, SIGNAL(sentMemoryUsage(int,double)), this, SLOT(memoryUsageChanged(int , double)));
+	connect(loop, SIGNAL(sentDiskUsage(int*,double*)), this, SLOT(diskUsageChanged(int* , double*)));
+
 }
 mainWindow::~mainWindow()
 {
@@ -34,3 +38,106 @@ mainWindow::~mainWindow()
 		delete loop;
 	}
 }
+int mainWindow::enableLogChanged()
+{
+	myUpdate.setEnableLog(ui.log->isChecked());
+	return 0;
+}
+int mainWindow::dataLogChanged()
+{
+	myUpdate.setDataLog(ui.dataLog->isChecked());
+	return 0;
+}
+int mainWindow::enableUpdateChanged()
+{
+	if (ui.update->isChecked())
+	{
+		emit stopLoop();
+	}
+	else
+	{
+		if (loop == nullptr)
+		{
+			loop = new mainLoop(nullptr, &myCPU, &myDisks, &myMemory, &myNetwork, &mySystem, &myUpdate);
+		}
+		loop->start();
+		connect(this, SIGNAL(stopLoop()), loop, SLOT(stopLoop()));
+	}
+	return 0;
+}
+int mainWindow::diskUsageWarningChanged()
+{
+	myUpdate.setDiskUsageWarning(ui.disk->isChecked());
+	return 0;
+}
+int mainWindow::memoryUsageWarningChanged()
+{
+	myUpdate.setMemoryUsageWarning(ui.memory->isChecked());
+	return 0;
+}
+int mainWindow::cpuUsageWarningChanged()
+{
+	myUpdate.setCpuUsageWarning(ui.cpu->isChecked());
+	return 0;
+}
+void mainWindow::diskUsageLimit()
+{
+	myUpdate.setDiskUsageWarning(ui.diskLimit->value());
+}
+void mainWindow::memoryUsageLimit()
+{
+	myUpdate.setMemoryUsageWarning(ui.memoryLimit->value());
+}
+void mainWindow::cpuUsageLimit()
+{
+	myUpdate.setCpuUsageWarning(ui.cpuLimit->value());
+}
+
+
+void mainWindow::cpuUsageChanged(int usage)
+{
+	//qDebug() << "A";
+}
+void mainWindow::diskIOChanged(io diskIO)
+{
+
+}
+void mainWindow::diskReadChanged(io read)
+{
+
+}
+void mainWindow::diskWriteChanged(io write)
+{
+
+}
+void mainWindow::netDownloadChanged(io download)
+{
+
+}
+void mainWindow::netUploadChanged(io upload)
+{
+
+}
+void mainWindow::runningTimeChanged(runningTime t)
+{
+
+}
+void mainWindow::memoryUsageChanged(int usage, double freespace)
+{
+
+}
+void mainWindow::diskUsageChanged(int* usage, double* freeSpace)
+{
+
+}
+
+//
+//{
+//
+//		<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
+//		<html><head><meta name = "qrichtext" content = "1" / ><meta charset = "utf-8" / ><style type = "text/css">
+//		p, li{ white - space: pre - wrap; }
+//		< / style>< / head><body style = " font-family:'Microsoft YaHei UI'; font-size:9pt; font-weight:400; font-style:normal;">
+//		<p style = " margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">L3缓存：< / p>
+//		< / body>< / html>
+//}
--- Windows/GUI/mainWindow.h
+++ Windows/GUI/mainWindow.h
@@ -1,4 +1,5 @@
 #include "libCommon.h"
+#include "classLib.h"
 #include "mainLoop.h"
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
@@ -8,6 +9,25 @@ class mainWindow : public QMainWindow
 
 signals:
     void stopLoop();
+public slots:
+	int enableLogChanged();
+	int enableUpdateChanged();
+	int dataLogChanged();
+	int diskUsageWarningChanged();
+	int memoryUsageWarningChanged();
+	int cpuUsageWarningChanged();
+	void diskUsageLimit();
+	void memoryUsageLimit();
+	void cpuUsageLimit();
+	void cpuUsageChanged(int usage);
+	void diskIOChanged(io diskIO);
+	void diskReadChanged(io read);
+	void diskWriteChanged(io write);
+	void netDownloadChanged(io download);
+	void netUploadChanged(io upload);
+	void runningTimeChanged(runningTime t);
+	void memoryUsageChanged(int usage, double freespace);
+	void diskUsageChanged(int* usage, double* freeSpace);
 public:
     mainWindow(QWidget *parent = Q_NULLPTR);
     ~mainWindow();
@@ -19,7 +39,7 @@ private:
 	memory myMemory;
 	network myNetwork;
 	operatingSystem mySystem;
-	mainLoop* loop = nullptr; 
+	mainLoop* loop = nullptr;
 };
 #endif 
 
